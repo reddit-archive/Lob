6df954096e95d20e2c8d555557241784779acf6a
David Forsythe <dforsythe@gmail.com>
2015-12-21 12:59:03 -0800

    kill old volley

--- lob/src/main/java/org/dforsyth/android/lob/ObjectRequest.java
+++ lob/src/main/java/org/dforsyth/android/lob/ObjectRequest.java
@@ -38,6 +38,7 @@ import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
 
 import org.dforsyth.android.lob.encoders.DecodeError;
